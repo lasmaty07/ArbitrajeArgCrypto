dfd917ec384532325a5e9f752b72d31939ce7173
Matt <matias.brigante@gmail.com>
2021-03-02 01:12:14 -0300

    add config json and secrets

--- main.py
+++ main.py
@@ -1,58 +1,103 @@
-import requests,os,json,time
+import requests,os,json,logging,sys
 from pathlib import Path
 from dotenv import load_dotenv
 
+LOG_FILENAME = 'Crypto.log'
+LOG_LEVEL=logging.INFO
+
+logging.basicConfig(filename=LOG_FILENAME,level=LOG_LEVEL)
+
+
 basepath = Path()
 basedir = str(basepath.cwd())
-envars = basepath.cwd() / 'config.env'
-load_dotenv(envars)
+envars = basepath.cwd() / 'SECRETS.env'
 
-_url = os.getenv("URL_CRYPTOYA")
-_coins = os.getenv("COINS").split(',')
+try:
+  load_dotenv(envars)
+except Exception as e:
+  logging.error(e)
+
+#load secrets
 _bot_token = os.getenv("BOT_TOKEN")
 _bot_chatID = os.getenv("BOT_CHAT_ID")
-_percentageConfig = float(os.getenv("PERCENTAGE"))
-_volumen = os.getenv("VOLUMEN")
+
+try:
+  with open('config.json', 'r') as f:
+    config = json.load(f)
+    logging.info("Config loaded")
+except IOError as e:
+  logging.error(e)
+
+#load config into vars
+_url = config["url"]
+_coins = config["coins"]
+_percentageConfig = float(config["percentage"])
+_exchangesEnabled = config["exchanges"]
+
 
 def getCotizacion(coin,fiat,volumen):
-  response = requests.get(_url + '/api/' + coin + '/'+ fiat+ '/' + str(volumen))
+  
+  try:
+    response = requests.get(_url + '/api/' + coin + '/'+ fiat+ '/' + str(volumen))
+  except Exception as e:
+    logging.error(e)  
+
   if response:
     exchanges = json.loads(response.text)
 
-    # TO-DO HORRIBLE
+    # init vars
     _ask = {'exchange':'','price':''}
     _bid = {'exchange':'','price':''}
-    _ask['exchange'] = 'argenbtc'
-    _bid['exchange'] = 'argenbtc'
+    _ask['exchange'] = list(exchanges)[0]
+    _bid['exchange'] = list(exchanges)[0]
     _ask['price'] = exchanges[list(exchanges)[0]]['totalAsk']
     _bid['price'] = exchanges[list(exchanges)[0]]['totalBid']
 
     # set bet prices
     for exchange in exchanges:
-      if _ask['price']> exchanges[exchange]['totalAsk']:
+      if _ask['price'] > exchanges[exchange]['totalAsk'] and exchange in _exchangesEnabled:
         _ask['price'] = exchanges[exchange]['totalAsk']
         _ask['exchange'] = exchange
-      if _bid['price']< exchanges[exchange]['totalBid']:
+      if _bid['price'] < exchanges[exchange]['totalBid'] and exchange in _exchangesEnabled:
         _bid['price'] = exchanges[exchange]['totalBid']
         _bid['exchange'] = exchange
 
     #set %
-    percentage = float(_ask['price'])-float(_bid['price'])*100/float(_bid['price'])
+    percentage = float((_bid['price'])-float(_ask['price']))*100/float(_bid['price'])
 
     #check if worth notifing.    
-    if  _ask['price']<_bid['price']:# and percentage>=_percentageConfig:
-      print(f'Moneda: {coin} para valores de {volumen} con {percentage}%')
-      print('Comprar en ' + _ask['exchange'] + ' a: ' + str(_ask['price']))
-      print('Vender en ' + _bid['exchange'] + ' a: ' + str(_bid['price']))
-      print('spread= ' + str( float(_bid['price']) - float(_ask['price'])))
+    if  _ask['price']<_bid['price'] and percentage>=_percentageConfig:
+      logging.info(f'Moneda: {coin} para valores de {volumen} con {percentage}%')
+      logging.info('Comprar en ' + _ask['exchange'] + ' a: ' + str(_ask['price']))
+      logging.info('Vender en ' + _bid['exchange'] + ' a: ' + str(_bid['price']))
+      logging.info('spread= ' + str(float(_bid['price']) - float(_ask['price'])))
 
-      bot_message = f'Moneda: {coin} para valores de {volumen}\nComprar en ' + _ask['exchange'] + ' a: ' + str(_ask['price']) + '\nVender en ' + _bid['exchange'] + ' a: ' + str(_bid['price'])   
+      bot_message = f'Moneda: {coin} para valores de {volumen}\nComprar en **' + _ask['exchange'] + '** a: ' + str(_ask['price']) + '\nVender en **' + _bid['exchange'] + '** a: ' + str(_bid['price']) + '\n' + str(percentage) +'%'
       
       send_text = 'https://api.telegram.org/bot' + _bot_token + '/sendMessage?chat_id=' + _bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-      #response = requests.get(send_text)
-  
-while True:
-  print('Start Run')
+      try:
+        response = requests.get(send_text)
+      except Exception as e:
+        logging.error(e)
+
+def printInfo():
+  #TODO FIX this
+  print(_coins)
+  coins = ''
   for coin in _coins:
-    getCotizacion(coin,'ars',_volumen)
-  time.sleep(60)
+    coins += ',' + coin
+
+  print(f'Configured Coins: {coins}' )
+  exchanges = ','.join(_exchangesEnabled)
+  print(f'Configured Exchanges: {exchanges}')
+
+def main():
+  #printInfo()
+  for coin in _coins:
+    getCotizacion(coin['name'],'ars',coin['vol'])
+
+  sys.exit(0)
+
+
+if __name__ == '__main__':
+  main()
